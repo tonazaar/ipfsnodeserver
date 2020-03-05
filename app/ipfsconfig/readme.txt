a0f7da9c2e7e33fc07a8b6ab1ed42645a7edcb82
Ramesh BN <raga2560@yahoo.com>
2020-03-05 06:47:16 +0000

    update

---
+++ app/ipfsconfig/readme.txt
@@ -0,0 +1,17 @@
+Atomic
+- mutually exclusive
+
+private+public  (can together)
+private|public  (or)
+private++public (always together)
+private--public (always never)
+ST (some together)
+
+AT
+NT
+OR
+CT
+ST
+
+
+Multi_config
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express  = require('express');
+var fs  = require('fs');
+var https = require('https');
 var app      = express();
 var mongoose = require('mongoose');
 var logger = require('morgan');
@@ -11,6 +13,17 @@ var router = require('./app/routes');
 mongoose.connect(databaseConfig.url);
 
 app.listen(process.env.PORT || 8080);
+/*
+
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app)
+.listen(443, function () {
+  console.log('app listening on port 443! Go to https://localhost:443/')
+})
+
+*/
 console.log("App listening on port 8080");
 
 app.use(bodyParser.urlencoded({ extended: false })); // Parses urlencoded bodies
