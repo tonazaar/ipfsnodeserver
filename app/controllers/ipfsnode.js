0b5231475e6859ae7dadb230ad9877b8a4ba1ddd
Ubuntu <ubuntu@ip-172-31-5-90.ap-southeast-1.compute.internal>
2020-06-11 05:02:45 +0000

    update

--- app/controllers/ipfsnode.js
+++ app/controllers/ipfsnode.js
@@ -65,7 +65,7 @@ exports.getipfsconfig = async function(req, res, next){
     };
 
   nodetype = req.body.nodetype;
-  var userid = req.user.userid;
+  var userid = req.body.userid;
 
   if(req.body.nodetype == 'clusternode') {
      basepath = '/cluster'+req.body.userid;
